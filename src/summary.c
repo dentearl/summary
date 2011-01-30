dccfeffd56448e8a7df9f2615a736df99ba601c3
Dent Earl <dearl@soe.ucsc.edu>
2011-01-29 18:46:48 -0800

    added the 9 types from R, checked them against the R output, looks good.

--- src/summary.c
+++ src/summary.c
@@ -17,9 +17,79 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, a copy is available at
  *  http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ *  Thanks and credit are due to Ivan Frohne and Rob J Hyndman
+ *  who wrote summary() in R.
+ *  References:
+ *
+ *   Becker, R. A., Chambers, J. M. and Wilks, A. R. (1988) _The New S
+ *   Language_.  Wadsworth & Brooks/Cole.
+ *
+ *   Hyndman, R. J. and Fan, Y. (1996) Sample quantiles in statistical
+ *   packages, _American Statistician_, *50*, 361-365.
+ *
+THE FOLLOWING TEXT WAS REMOVED FROM THE QUANTILE DOCUMENTATION IN R:
+quantile returns estimates of underlying distribution quantiles based on one 
+or two order statistics from the supplied elements in x at probabilities in 
+probs. One of the nine quantile algorithms discussed in Hyndman and Fan 
+(1996), selected by type, is employed.
+
+All sample quantiles are defined as weighted averages of consecutive order 
+statistics. Sample quantiles of type i are defined by:
+
+Q[i](p) = (1 - gamma) x[j] + gamma x[j+1],
+
+where 1 <= i <= 9, (j-m)/n <= p < (j-m+1)/n, x[j] is the jth order statistic, 
+n is the sample size, the value of γ is a function of j = floor(np + m) and
+g = np + m - j, and m is a constant determined by the sample quantile type.
+
+Discontinuous sample quantile types 1, 2, and 3
+
+For types 1, 2 and 3, Q[i](p) is a discontinuous function of p, with m = 0 
+when i = 1 and i = 2, and m = -1/2 when i = 3.
+
+Type 1
+Inverse of empirical distribution function. gamma = 0 if g = 0, and 1 otherwise.
+Type 2
+Similar to type 1 but with averaging at discontinuities. gamma = 0.5 if g = 0, 
+and 1 otherwise.
+Type 3
+SAS definition: nearest even order statistic. gamma = 0 if g = 0 and j is even, 
+and 1 otherwise.
+
+Continuous sample quantile types 4 through 9
+
+For types 4 through 9, Q[i](p) is a continuous function of p, with gamma = g
+and m given below. The sample quantiles can be obtained equivalently by linear 
+interpolation between the points (p[k],x[k]) where x[k] is the kth order 
+statistic. Specific expressions for p[k] are given below.
+
+Type 4
+m = 0. p[k] = k / n. That is, linear interpolation of the empirical cdf.
+Type 5
+m = 1/2. p[k] = (k - 0.5) / n. That is a piecewise linear function where 
+the knots are the values midway through the steps of the empirical cdf. 
+This is popular amongst hydrologists.
+Type 6
+m = p. p[k] = k / (n + 1). Thus p[k] = E[F(x[k])]. This is used by Minitab 
+and by SPSS.
+Type 7
+m = 1-p. p[k] = (k - 1) / (n - 1). In this case, p[k] = mode[F(x[k])]. 
+This is used by S.
+Type 8
+m = (p+1)/3. p[k] = (k - 1/3) / (n + 1/3). Then p[k] =~ median[F(x[k])]. 
+The resulting quantile estimates are approximately median-unbiased regardless 
+of the distribution of x.
+Type 9
+m = p/4 + 3/8. p[k] = (k - 3/8) / (n + 1/4). The resulting quantile estimates 
+are approximately unbiased for the expected order statistics if x is normally 
+distributed.
+Further details are provided in Hyndman and Fan (1996) who recommended 
+type 8. The default method is type 7, as used by S and by R < 2.0.0.
  */
 #include <assert.h>
 #include <limits.h> /* INT_MAX, LINE_MAX */
+#include <float.h> /* DBL_EPSILON */
 #include <math.h>  /* pow() */
 #include <stdio.h>
 #include <stdlib.h>
@@ -42,7 +112,7 @@ void version(void){
 }
 
 void usage(void){
-   fprintf(stderr, "USAGE: summary takes input via stdin. --precision [0..9]\n\n");
+   fprintf(stderr, "USAGE: summary takes input via stdin, one number per line. --precision [0..9] --type [1..9]\n\n");
    exit(2);
 }
 
@@ -52,21 +122,29 @@ void memError(void){
 }
 
 void help(void){
-   printf( "  summary a program like the R function summary().                    \n"
-           "  $ echo -e '112\\n98.1\\n' | summary                                 \n"
+   printf( "  summary a program like the R function summary().\n"
+           "  $ echo -e '112\\n98.1\\n101\\n' | summary\n"
            "        n       Min.    1st Qu.     Median       Mean    3rd Qu.       Max.     Stdev.\n"
-           "        2    98.1000   101.5750   105.0500   105.0500   108.5250   112.0000     9.8288\n"
-           "  stdin: one number on each line of input, no puntuation, text or     \n"
+           "        3    98.1000    99.5500   101.0000   103.7000   106.5000   112.0000     7.3328\n\n"
+           "  stdin: one number on each line of input, no puntuation, text or\n"
            "  blank lines.  \n"
-           "  --precision [0..9] adjusts the number of decimals.\n"
-           "  Quartiles Method:\n"
-           "  We use R type 7, (in R try ?stats::quantile)\n"
-           "  Type 7 m = 1-p.  p[k] = (k - 1) / (n - 1).  In this case, p[k] =\n"
-           "  mode[F(x[k])].  This is used by S.\n"
+           " OPTIONS\n"
+           "   --precision [0..9] adjusts the number of decimals.\n"
+           "   --type [1..9] selects one of the nine quantile algorithms. 7 is default, inside of R\n"
+           "                 try ?stats::quantile to see details.\n\n"
+           " *  Thanks and credit are due to Ivan Frohne and Rob J Hyndman\n"
+           " *  who wrote summary() in R.\n"
+           " *  References:\n"
+           " *\n"
+           " *   Becker, R. A., Chambers, J. M. and Wilks, A. R. (1988) _The New S\n"
+           " *   Language_.  Wadsworth & Brooks/Cole.\n"
+           " *\n"
+           " *   Hyndman, R. J. and Fan, Y. (1996) Sample quantiles in statistical\n"
+           " *   packages, _American Statistician_, *50*, 361-365.\n\n"
            );
    exit(0);
 }
-void gatherOptions(int argc, char **argv, int *prec )
+void gatherOptions(int argc, char **argv, int *prec, int *type )
 {
    int c;
    while (1)
@@ -78,11 +156,12 @@ void gatherOptions(int argc, char **argv, int *prec )
                {"help", no_argument, 0, 'h'},
                {"version", no_argument, 0, 'v'},
                {"precision", required_argument, 0, 'p'},
+               {"type", required_argument, 0, 't'},
                {0, 0, 0, 0}
             };
          /* getopt_long stores the option index here. */
          int option_index = 0;
-         c = getopt_long(argc, argv, "p:",
+         c = getopt_long(argc, argv, "p:t:",
                          long_options, &option_index);
          /* Detect the end of the options. */
          if (c == -1){
@@ -95,6 +174,9 @@ void gatherOptions(int argc, char **argv, int *prec )
             case 'p':
                sscanf( optarg, "%d", prec );
                break;
+            case 't':
+               sscanf( optarg, "%d", type );
+               break;
             case 'h':
                help();
             case 'v':
@@ -106,7 +188,7 @@ void gatherOptions(int argc, char **argv, int *prec )
                abort ();
             }
       }
-   if ((*prec < 0) || (*prec > 9 ))
+   if ((*prec < 0) || (*prec > 9 ) || (*type < 1) || (*type > 9))
       usage();
 }
 
@@ -201,6 +283,12 @@ void copyArraySubset( double *x, double *y, int start, int end){
       y[ j++ ] = x [ i ];
 }
 
+void copyArrayIntoCenter( double *x, double *y, int n ){
+   int i;
+   for (i = 0; i < n; ++i)
+      y[ i+2 ] = x [ i ];
+}
+
 double Q1( double *x, int n ){
    /* Q1 uses the median of the lower half approach
       to find the first quartile.
@@ -235,7 +323,7 @@ double Q3( double *x, int n ){
    }
 }
 
-double quantile( double *x, int n, double p){
+double quantile( double *x, int n, double p, int type){
    /* Q(p) = (1 - gamma) * x[j] + gamma * x[j+1],
       where ( j - m ) / n <= p < ( j - m + 1 ) / n, x[j] is the 
       jth order statistic, n is the sample size,
@@ -245,13 +333,93 @@ double quantile( double *x, int n, double p){
       packages R and S.
     */
    // this code is refactored from the R function summary.R
-   double index = (n - 1.0) * p; // R version : 1.0 + (n - 1.0) * p
-   int lo = floor( index );
-   int hi = ceil( index );
-   double qs = x[ lo ];
-   double h = index - lo;
-   qs = (1 - h) * qs + h * x[ hi ];
-   return qs;
+   double a, b, nppm, h, qs;
+   int j;
+   if ( type == 7 ){
+      double index = (n - 1.0) * p; // R version : 1.0 + (n - 1.0) * p
+      int lo = floor( index );
+      int hi = ceil( index );
+      qs = x[ lo ];
+      h = index - lo;
+      qs = (1 - h) * qs + h * x[ hi ];
+      return qs;
+   }else{
+      if ( type <= 3 ){
+         /* Types 1..3 are discontinuous sample quantiles */
+
+         if (type == 3)
+            nppm = n * p - 0.5;
+         else
+            nppm = n * p;
+         j = floor( nppm );
+         switch( type ){
+         case 1:
+            if ( nppm > j )
+               h = 1.0;
+            else
+               h = 0.0;
+            break;
+         case 2:
+            if ( nppm > j )
+               h = 1.0;
+            else
+               h = 0.5;
+            break;
+         case 3:
+            if (( nppm == j ) && ( j % 2 ) == 0 )
+               h = 0.0;
+            else
+               h = 1.0;
+            break;
+         }
+      }else{
+         /* Types 4 through 9 are continuous sample quantiles */
+         switch( type ){
+         case 4:
+            a = 0.0;
+            b = 1.0;
+            break;
+         case 5:
+            a = 0.5;
+            b = 0.5;
+            break;
+         case 6:
+            a = 0.0;
+            b = 0.0;
+            break;
+         /* case 7 has alpha = beta = 1 */
+         case 8:
+            a = 1.0 / 3.0;
+            b = 1.0 / 3.0;
+            break;
+         case 9:
+            a = 3.0 / 8.0;
+            b = 3.0 / 8.0;
+            break;
+         }
+         double fuzz = 4.0 * DBL_EPSILON;
+         nppm = a + p * ( n + 1 - a - b );
+         j = floor( nppm + fuzz );
+         h = (double)nppm - (double)j;
+         if ( fabs(h) < fuzz )
+            h = 0.0;
+      }
+      double *y = NULL;
+      y = calloc( n + 4, sizeof(double));
+      if ( y == NULL )
+         memError();
+      copyArrayIntoCenter( x, y, n );
+      y[ 0 ]     = x[ 0 ];
+      y[ 1 ]     = x[ 0 ];
+      y[ n + 3 ] = x[ n ];
+      y[ n + 4 ] = x[ n ];
+      double qs = y[ j + 1 ];
+      if ( h == 1.0)
+         qs = y[ j + 2 ];
+      if (( h > 0.0 ) && ( h < 1.0 ))
+         qs = (1.0 - h ) * y[ j + 1 ] + h * y[ j + 2 ];
+      return qs;
+   }
 }
 
 double sampStdev( double *x, int n, double ave){
@@ -265,14 +433,14 @@ double sampStdev( double *x, int n, double ave){
    return sqrt( s );
 }
 
-void summarize( double *x, int n, double ave, int prec ){
+void summarize( double *x, int n, double ave, int prec, int type ){
    qsort( x, n, sizeof(double), dbl_cmp );
    printf( "%9s  %9s  %9s  %9s  %9s  %9s  %9s  %9s\n", 
            "n", "Min.", "1st Qu.", "Median", "Mean", "3rd Qu.", 
            "Max.", "Stdev." );
    printf( "%9d  %9.*f  %9.*f  %9.*f  %9.*f  %9.*f  %9.*f  %9.*f\n", 
-           n, prec, x[0], prec, quantile( x, n, 0.25 ), prec, median(x, n, 0),
-           prec, ave, prec, quantile( x, n, 0.75 ), 
+           n, prec, x[0], prec, quantile( x, n, 0.25, type ), prec, median(x, n, 0),
+           prec, ave, prec, quantile( x, n, 0.75, type ), 
            prec, x[n-1], prec, sampStdev(x, n, ave) );
 }
 
@@ -282,7 +450,8 @@ int main (int argc, char **argv)
    double d;
    int n = 0;
    int prec = 4;
-   gatherOptions( argc, argv, &prec );
+   int type = 7;
+   gatherOptions( argc, argv, &prec, &type );
    doubleList *list = NULL;
    initList( list );
    while ( scanf("%lf", &d) == 1 ){
@@ -292,7 +461,7 @@ int main (int argc, char **argv)
    double *array;
    array = listToArray( list, n );
    releaseList( list );
-   summarize( array, n, ave, prec );
+   summarize( array, n, ave, prec, type );
    free( array );
    return 0;
 }
